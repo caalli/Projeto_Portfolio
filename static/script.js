5041f780d184b63fd5c21b2be5c654f4607c8d8f
Carlos Lima <caalli_lima@hotmail.com>
2023-02-21 16:02:35 -0300

    Atualizações 21/02/2023

--- static/script.js
+++ static/script.js
@@ -10,3 +10,19 @@ menuMobile.addEventListener("click", () => {
   body.classList.toggle("menu-nave-active");
 
 });
+
+
+/* Fecha o menu quando clicar em algum item e muda o icone para list */
+
+const navItem = document.querySelectorAll(".nav-item");
+
+navItem.forEach((item) => {
+
+  item.addEventListener("click", () => {
+    if (body.classList.contains("menu-nave-active")) {
+      body.classList.remove("menu-nave-active")
+      menuMobile.classList.replace("bi-x", "bi-list");
+      
+    }
+})
+})
